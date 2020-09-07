ef13b19ab62346266af2bd855b09f5f1f637b056
kevin lopes <kevinlp@live.com>
2020-09-07 16:25:28 -0300

    Atividade 1 - alterações comentadas

--- app/src/main/java/com/uniso/lpdm/restaurango/MainActivity.java
+++ app/src/main/java/com/uniso/lpdm/restaurango/MainActivity.java
@@ -12,16 +12,22 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //Código necessario para exibir a view inicial e o "header em cima"
     protected void onCreate(Bundle savedInstanceState) {
+        //associa o layout em activity_main.xml com o que será exibido em tela
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Cria a toolbar com o nome da aplicação em cima
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 
+    //Função que será chamada ao clicar no botão "check_hours"
     public void onClickCheckHours(View view){
+        //Encontra a view pelo seu ID
         TextView hours = findViewById(R.id.text_view_total_hours);
+        //Chama a função que troca o texto
         hours.setText("Várias horas mesmo");
     }
 
