0357e9702bde5edd65b497e31674694969bd0b1b
kevin lopes <kevinlp@live.com>
2020-09-07 16:15:15 -0300

    Atividade 1 - Inserção de botões

--- app/src/main/java/com/uniso/lpdm/restaurango/MainActivity.java
+++ app/src/main/java/com/uniso/lpdm/restaurango/MainActivity.java
@@ -1,17 +1,13 @@
 package com.uniso.lpdm.restaurango;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
+
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,35 +18,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onClickCheckHours(View view){
+        TextView hours = findViewById(R.id.text_view_total_hours);
+        hours.setText("Várias horas mesmo");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onClickRegisterOrder(View view){
+        TextView order = findViewById(R.id.text_view_order);
+        order.setText("Registrando pedido....");
     }
+
+
+
 }
